5a49cfb526b5fe446da22a63347d66d8d32a7288
glovekik <161479252+glovekik@users.noreply.github.com>
2024-03-26 11:55:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app = express();
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on the port number ${PORT}`));
+
+app.get('/klef', async function(req, res){
+    res.json("Koneru Lakshmaiah Education Foundation");
+});
